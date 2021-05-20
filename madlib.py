5466b8819fa033a30d5418b7836870c675049e3c
S.M.Fatima <s.m.fatima13@gmail.com>
2021-05-20 12:37:12 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#### madlib game
+
+Adj = input("Adjective: ")
+verb = input("Verb: ")
+verb1 = input("Verb: ")
+
+
+madlib = f"why the weather today is {Adj}, I am feeling very {verb}. Hope tomorrow will {verb}"
+
+print(madlib)
